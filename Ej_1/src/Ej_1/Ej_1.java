dffb9b857ecd2f377853c767c09df26c32e472ce
etosas <etosas746@alumnos.iau.edu.ar>
2018-08-16 10:21:51 -0300

    Factorial con Visualisacion

---
+++ Ej_1/src/Ej_1/Ej_1.java
@@ -0,0 +1,29 @@
+package Ej_1;
+
+import java.util.Scanner;
+
+public class Ej_1 {
+	
+	private static Scanner scanner;
+
+	public static void main(String[] args) {
+		System.out.println("Ingrese un numero");
+		
+		scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		
+		visualizarRecursion(num);
+
+	}
+	
+	public static long visualizarRecursion(int num) {
+		
+		
+		if(num == 1) {
+			return 1;
+		}
+		 
+		System.out.println("El factorial de: "+num+" es: " + "=" + num + "*" + (num-1)+"!"  );
+		return num * visualizarRecursion(num-1);
+	}
+}

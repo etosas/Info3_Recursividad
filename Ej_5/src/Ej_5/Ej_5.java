7f2f275226c7c1e5855c68dd63e90aaea87a0442
etosas <etosas746@alumnos.iau.edu.ar>
2018-08-16 09:14:22 -0300

    e

--- Ej_5/src/Ej_5/Ej_5.java
+++ Ej_5/src/Ej_5/Ej_5.java
@@ -4,7 +4,16 @@ public class Ej_5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] info = {3,5,6,7,9,11,13,15};
+		mostrarMatriz(info, info.length);
+	}
+	
+	public static void mostrarMatriz(int [] datos,int tam){
+		if(tam != 0) {
+			System.out.println(datos[tam-1]);
+			mostrarMatriz(datos, tam-1);
+		}
+		
 	}
 
 }

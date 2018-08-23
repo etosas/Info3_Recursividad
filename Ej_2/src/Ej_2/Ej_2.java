c166fffce4675245658af3e2f8937a27b7717ce8
etosas <etosas746@alumnos.iau.edu.ar>
2018-08-22 22:54:47 -0300

    Metodo de potencia recursivo

--- Ej_2/src/Ej_2/Ej_2.java
+++ Ej_2/src/Ej_2/Ej_2.java
@@ -3,16 +3,21 @@ package Ej_2;
 public class Ej_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double resultado;
+		
+		resultado = power (2.2,2);
+		
+		System.out.println(resultado);
+		
 
 	}
 	
-	public static double power(int base, int exponente) {
-		if(exponente==1) {
-			
+	public static double power(double base, double exponente) {
+		if(exponente==0) {
+			return 1;
 		}
 		
-		return 0.0;
+		return base * power(base,exponente -1) ;
 	}
 
 }

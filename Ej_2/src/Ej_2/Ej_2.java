c67a0438930dc5bae2f9d81d24827eec5fb8c183
etosas <etosas746@alumnos.iau.edu.ar>
2018-08-22 22:23:53 -0300

    Ejercicio 2

---
+++ Ej_2/src/Ej_2/Ej_2.java
@@ -0,0 +1,18 @@
+package Ej_2;
+
+public class Ej_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static double power(int base, int exponente) {
+		if(exponente==1) {
+			
+		}
+		
+		return 0.0;
+	}
+
+}
